40388d5b722587e2b88652e48e1569126113aebc
Rajesh Jangam <rajesh@altizon.com>
2015-11-06 16:26:47 +0530

    Changed the SDK to use Thing instead of DataStream nomenclature

--- java/src/main/java/com/altizon/agent/SampleAgent.java
+++ java/src/main/java/com/altizon/agent/SampleAgent.java
@@ -2,9 +2,9 @@
 package com.altizon.agent;
 
 import io.datonis.sdk.AliotGateway;
-import io.datonis.sdk.DataStream;
 import io.datonis.sdk.InstructionHandler;
-import io.datonis.sdk.exception.IllegalStreamException;
+import io.datonis.sdk.Thing;
+import io.datonis.sdk.exception.IllegalThingException;
 import io.datonis.sdk.message.AlertType;
 import io.datonis.sdk.message.AliotInstruction;
 
@@ -17,7 +17,7 @@ import org.slf4j.LoggerFactory;
 import com.sun.management.OperatingSystemMXBean;
 
 /**
- * A sample program that sends a stream of events to Datonis
+ * A sample program that sends events to Datonis
  * 
  * @author Ranjit Nair (ranjit@altizon.com)
  */
@@ -25,7 +25,7 @@ public class SampleAgent {
     private static final Logger logger = LoggerFactory.getLogger(SampleAgent.class);
     private static int NUM_EVENTS = 50;
 
-    private DataStream stream;
+    private Thing thing;
     private AliotGateway gateway;
 
     private SampleAgent() {
@@ -38,9 +38,9 @@ public class SampleAgent {
      * Main Entry Point in the program
      * 
      * @param args
-     * @throws IllegalStreamException
+     * @throws IllegalThingException
      */
-    public static void main(String[] args) throws IllegalStreamException {
+    public static void main(String[] args) throws IllegalThingException {
         // First make sure you create an aliot.properties file using your downloaded access and secret keys.
         // A sample file should be available with this package
         // The keys are available in the Datonis platform portal (Please contact your Account Administrator).
@@ -54,7 +54,7 @@ public class SampleAgent {
             logger.info("Agent started Successfully, setting up bidirectional communication");
 
             // Setup bidirectional communication - Needs to use MQTT mode for communication
-            // Also, it needs your stream to be configured accordingly in the start gateway
+            // Also, it needs your thing to be configured accordingly in the start gateway
             agent.setupBiDirectionalCommunication();
 
             logger.info("Bidirectional communication is setup with Datonis");
@@ -65,7 +65,7 @@ public class SampleAgent {
                 agent.transmitDemoAlerts();
             }
 
-            // Stream a few sample simulated data events
+            // Send a few sample simulated data events
             logger.info("Transmitting Demo Data");
             agent.transmitData();
             logger.info("Transmitted Demo data");
@@ -79,32 +79,32 @@ public class SampleAgent {
 
     private boolean startGateway() {
         try {
-            // Decide what the metadata format of your stream should be. 
-            // This will show up as the stream parameters that you are pushing on Datonis.
+            // Decide what the metadata format of your thing should be.
+            // This will show up as the thing parameters that you are pushing on Datonis.
             // In this example we will be pushing cpu and memory utilization.
             // We therefore declare them in a JSON schema format.
             // Obviously escaped so that java does not complain
             String metadata = "{\"cpu\": {\"type\":\"number\"}, \"mem\": {\"type\":\"number\"}}";
 
-            // Create a Stream Object.
-            // It is extremely important to have unique Stream keys or collisions may occur and data could get overwritten.
-            // Please create a DataStream on the Datonis portal and use the key here.
-            // Stream names are non-unique, however uniqueness is recommended.
-            // Use a logical 'type' to describe the Data Stream. For instance, System Monitor in this case.
-            // Multiple streams can exist for a type.
-            // This constructor will throw an illegal stream exception if conditions are not met.
-            stream = new DataStream("stream_key", "SysMon", "System Monitor", "A monitor for CPU and Memory", metadata);
+            // Create a Thing Object.
+            // It is extremely important to have unique Thing keys or collisions may occur and data could get overwritten.
+            // Please create a Thing on the Datonis portal and use the key here.
+            // Thing names are non-unique, however uniqueness is recommended.
+            // Use a logical 'type' to describe the Thing. For instance, System Monitor in this case.
+            // Multiple things can exist for a type.
+            // This constructor will throw an illegal thing exception if conditions are not met.
+            thing = new Thing("thing_key", "SysMon", "System Monitor", "A monitor for CPU and Memory", metadata);
 
-            // Comment the line earlier and un-comment this line if you want this stream to be bi-directional i.e. supports receiving instructions (Note: Only works with MQTT/MQTTs)
-            // stream = new DataStream("stream_key", "SysMon", "System Monitor", "A monitor for CPU and Memory", metadata, true);
+            // Comment the line earlier and un-comment this line if you want this thing to be bi-directional i.e. supports receiving instructions (Note: Only works with MQTT/MQTTs)
+            // thing = new Thing("thing_key", "SysMon", "System Monitor", "A monitor for CPU and Memory", metadata, true);
 
-            // You can register multiple streams and send data for them.
-            // First add the streams and then call register.
-            // In this case, there is only a single stream object.
-            gateway.addDataStream(stream);
+            // You can register multiple things and send data for them.
+            // First add the things and then call register.
+            // In this case, there is only a single thing object.
+            gateway.addThing(thing);
 
             gateway.start();
-        } catch (IllegalStreamException e) {
+        } catch (IllegalThingException e) {
             logger.error("Could not start the aliot gateway: ", e.getMessage(), e);
             return false;
         }
@@ -120,11 +120,11 @@ public class SampleAgent {
 
             @Override
             public void handleInstruction(AliotGateway gateway, AliotInstruction instruction) {
-                logger.info("Received instruction for stream: " + instruction.getStreamKey()
+                logger.info("Received instruction for thing: " + instruction.getThingKey()
                         + " from Datonis: " + instruction.getInstruction().toJSONString());
                 JSONObject data = new JSONObject();
                 data.put("execution_status", "success");
-                if (!gateway.transmitAlert(instruction.getAlertKey(), instruction.getStreamKey(), AlertType.WARNING, "Demo warning, instruction received and logged!", data)) {
+                if (!gateway.transmitAlert(instruction.getAlertKey(), instruction.getThingKey(), AlertType.WARNING, "Demo warning, instruction received and logged!", data)) {
                     logger.error("Could not send Acknowlegement for instruction back to datonis.");
                 } else {
                     logger.info("Sent an instruction acknowlegement back to Datonis!");
@@ -149,7 +149,7 @@ public class SampleAgent {
         JSONObject data = new JSONObject();
         data.put("demoKey", "demoValue");
 
-        if (!gateway.transmitAlert(stream.getKey(), alertType, "This is an example " + alertType.toString() + " alert!", data)) {
+        if (!gateway.transmitAlert(thing.getKey(), alertType, "This is an example " + alertType.toString() + " alert!", data)) {
             logger.info("Sent example " + alertType.toString() + " alert!");
         } else {
             logger.error("Could not send example " + alertType.toString() + " alert");
@@ -163,7 +163,7 @@ public class SampleAgent {
         transmitAlert(AlertType.CRITICAL);
     }
 
-    public void transmitData() throws IllegalStreamException {
+    public void transmitData() throws IllegalThingException {
         int heartbeat = 0;
         for (int count = 1; count <= NUM_EVENTS; count++) {
             // Construct the JSON packet to be sent. This has to match the
@@ -171,8 +171,8 @@ public class SampleAgent {
             JSONObject data = getSystemInfo();
             // Transmit the data. There is also a method to
             // specify the timestamp of the data packet if the transmission is delayed. 
-            // The syntax is gateway.transmitData(stream, data, timestamp)
-            if (!gateway.transmitData(stream, data)) {
+            // The syntax is gateway.transmitData(thing, data, timestamp)
+            if (!gateway.transmitData(thing, data)) {
                 logger.warn("Could not transmit packet : " + count + " value " + data.toJSONString());
             }
 
