200d5fa2182e704635bda17724c4343095d8471f
ranjitnair77 <ranjit@altizon.com>
2017-05-02 15:13:49 +0530

    Update SampleAgent.java
    Cleaned up the SampleAgent with code commenting. Moved a few functions around for better readability.

--- java/src/main/java/com/altizon/agent/SampleAgent.java
+++ java/src/main/java/com/altizon/agent/SampleAgent.java
@@ -1,4 +1,3 @@
-
 package com.altizon.agent;
 
 import io.datonis.sdk.AliotGateway;
@@ -14,117 +13,117 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 /**
  * A sample program that sends events to Datonis
  * 
- * @author Ranjit Nair (ranjit@altizon.com)
+ * @author Rajesh Jangam (rajesh_jangam@altizon.com)
  */
-public class SampleAgent {
+public class SampleAgent
+{
     private static final Logger logger = LoggerFactory.getLogger(SampleAgent.class);
-    private static int NUM_EVENTS = 50;
+    private static int NUM_EVENTS = 5;
 
     private Thing thing;
     private AliotGateway gateway;
 
-    private SampleAgent() {
-        // Construct the Gateway that helps us send various types of events to
-        // Datonis
+    private SampleAgent()
+    {
+        // The AliotGateway class is the main class for sending data to Datonis. Create an instance and store it for
+        // future use.
         this.gateway = new AliotGateway();
     }
 
-    /**
-     * Main Entry Point in the program
-     * 
-     * @param args
-     * @throws IllegalThingException
-     */
-    public static void main(String[] args) throws IllegalThingException {
-        // First make sure you create an aliot.properties file using your downloaded access and secret keys.
+    public static void main(String[] args) throws IllegalThingException
+    {
+        // First make sure you create an aliot.properties file using your access and secret keys.
         // A sample file should be available with this package
-        // The keys are available in the Datonis platform portal (Please contact your Account Administrator).
+        // The keys are available in the Datonis platform portal
 
         logger.info("Starting the Sample Aliot Agent");
         // Create and Initialize the agent
         SampleAgent agent = new SampleAgent();
 
         // Start the Gateway
-        if (agent.startGateway()) {
-            logger.info("Agent started Successfully, setting up bidirectional communication");
-
-            // Setup bidirectional communication - Needs to use MQTT mode for communication
-            // Also, it needs your thing to be configured accordingly in the start gateway
-            agent.setupBiDirectionalCommunication();
-
-            logger.info("Bidirectional communication is setup with Datonis");
-
-            // Enable this condition if you want to send a few example alerts
-            if (false) {
-                logger.info("Transmitting Demo Alerts");
-                agent.transmitDemoAlerts();
-            }
-
-            // Send a few sample simulated data events
-            logger.info("Transmitting Demo Data");
-            agent.transmitData();
-            logger.info("Transmitted Demo data");
-
-            logger.info("Exiting");
-            agent.stopGateway();
-        } else {
-            logger.error("Could not start Sample Aliot Agent. Please check aliot.log for more details");
+        if (!agent.startGateway())
+        {
+            logger.info("Could not start the agent. Exiting");
+            return;
         }
+
+        logger.info("Agent started Successfully, setting up bidirectional communication");
+
+        // Setup bidirectional communication - Needs to use MQTTS/MQTT mode for communication.
+        // Ensure that the 'protocol' property is set to 'mqtts' in the aliot.properties file
+        agent.setupBiDirectionalCommunication();
+
+        // Send a few sample simulated data events
+        logger.info("Transmitting Data");
+        agent.transmitData();
+
+        // Transmit a set of sample alerts.
+        logger.info("Transmitting Alerts");
+        agent.transmitAlerts();
+
+        // Transmit a waypoint
+        logger.info("Transmitting a waypoint");
+        agent.transmitWaypoint();
+
+        logger.info("Exiting");
+        agent.stopGateway();
     }
 
-	private boolean startGateway() {
-        try {
-            // Decide what the metadata format of your thing should be.
-            // This will show up as the thing parameters that you are pushing on Datonis.
-            // In this example we will be pushing cpu and memory utilization.
-            // We therefore declare them in a JSON schema format.
-            // Obviously escaped so that java does not complain
-            
+    private boolean startGateway()
+    {
+        try
+        {
 
-            // Create a Thing Object.
-            // It is extremely important to have unique Thing keys or collisions may occur and data could get overwritten.
-            // Please create a Thing on the Datonis portal and use the key here.
-            // Thing names are non-unique, however uniqueness is recommended.
-            // Use a logical 'type' to describe the Thing. For instance, System Monitor in this case.
-            // Multiple things can exist for a type.
-            // This constructor will throw an illegal thing exception if conditions are not met.
+            // Create a Thing Object.It is extremely important use a proper Thing key or collisions may occur and data
+            // could get overwritten.
+            // Please create a Thing on the Datonis portal and use the key here. Thing can be non-unique, however
+            // uniqueness is recommended.
             thing = new Thing("<Your Thing Key>", "LivingRoom", "The living room temperature and humidity device.");
 
-            // Comment the line earlier and un-comment this line if you want this thing to be bi-directional i.e. supports receiving instructions (Note: Only works with MQTT/MQTTs)
-            // thing = new Thing("Your Thing's key goes here", "SysMon", "A monitor for CPU and Memory", true);
-
-            // You can register multiple things and send data for them.
-            // First add the things and then call register.
-            // In this case, there is only a single thing object.
+            // You can register multiple things and send data for them. First add the things and then call register.
+            // In this case, there is only a single thing object so only a single Thing gets added.
             gateway.addThing(thing);
 
+            // Now start the gateway which automatically registers all the Things. You are now ready to transmit data.
             gateway.start();
-        } catch (IllegalThingException e) {
+        }
+        catch (IllegalThingException e)
+        {
             logger.error("Could not start the aliot gateway: ", e.getMessage(), e);
             return false;
         }
         return true;
     }
 
-    private void stopGateway() {
-        gateway.stop();
-    }
-
-    private void setupBiDirectionalCommunication() {
+    private void setupBiDirectionalCommunication()
+    {
+        // Register an instruction handler with the gatweay. Whenever Datonis sends an instruction down, the instruction
+        // handler is automatically invoked.
         gateway.setInstructionHandler(new InstructionHandler() {
 
             @Override
-            public void handleInstruction(AliotGateway gateway, AliotInstruction instruction) {
-                logger.info("Received instruction for thing: " + instruction.getThingKey()
-                        + " from Datonis: " + instruction.getInstruction().toJSONString());
+            public void handleInstruction(AliotGateway gateway, AliotInstruction instruction)
+            {
+
+                logger.info("Received instruction for thing: " + instruction.getThingKey() + " from Datonis: " + instruction.getInstruction().toJSONString());
+                // The instruction.getInstruction() call gives details of the passed instruction. You can now take the
+                // appropriate action based on that instruction. `
+
                 JSONObject data = new JSONObject();
+                // When the instruction is successfully executed, you should send an execution status of success back to
+                // the platform. You can also put additional parameters in there which provides additional context about
+                // the instruction executed.
                 data.put("execution_status", "success");
-                if (!gateway.transmitAlert(instruction.getAlertKey(), instruction.getThingKey(), AlertType.WARNING, "Demo warning, instruction received and logged!", data)) {
+                if (!gateway.transmitAlert(instruction.getAlertKey(), instruction.getThingKey(), AlertType.WARNING, "Demo warning, instruction received and logged!", data))
+                {
                     logger.error("Could not send Acknowlegement for instruction back to datonis.");
-                } else {
+                }
+                else
+                {
                     logger.info("Sent an instruction acknowlegement back to Datonis!");
                 }
             }
@@ -134,83 +133,102 @@ public class SampleAgent {
     private int generateValue(int min, int max)
     {
         return new Random().nextInt(max - min + 1) + min;
-        
+
     }
-    
-    private JSONObject getMetric() {
-        JSONObject obj = new JSONObject();
-        obj.put("temperature", generateValue(0, 35));
-        obj.put("humidity", generateValue(50, 95));
-        return obj;
+
+    private void transmitData() throws IllegalThingException
+    {
+        for (int count = 1; count <= NUM_EVENTS; count++)
+        {
+            // Construct the JSON packet to be sent. This has to correspond to the metric names. For instance, you have
+            // created a ThingTemplate with two metrics, temperature and humidity. You are now sending metrics for the
+            // Things that belong to that Thing Template.The metric names that you use here must match the metric names
+            // that you have set up in the Thing Template.
+            // The format of the data is JSON. We thus build a JSON object.
+            JSONObject data = new JSONObject();
+
+            // Put a random value between 0 to 35 for temperature
+            data.put("temperature", generateValue(0, 35));
+            // Put a random value between 50 to 95 for humidity
+            data.put("humidity", generateValue(50, 95));
+
+            // Compress and transmit the data. Compressing is highly recommended for efficient transmit.
+            if (!gateway.transmitCompressedData(thing, data, null))
+            {
+                logger.warn("Could not transmit packet : " + count + " value " + data.toJSONString());
+            }
+
+            try
+            {
+                Thread.currentThread().sleep(5000);
+            }
+            catch (InterruptedException e)
+            {
+                e.printStackTrace();
+            }
+
+        }
+
     }
-    
-    private JSONArray createWaypoint(double latitude, double longitude) {
+
+    private void transmitAlert(AlertType alertType, String message)
+    {
+        // You can send additional information along with the alert in a JSON format.
+        JSONObject alertParams = new JSONObject();
+        alertParams.put("message", message);
+
+        if (!gateway.transmitAlert(thing.getKey(), alertType, "This is an example " + alertType.toString() + " alert!", alertParams))
+        {
+            logger.error("Could not send example " + alertType.toString() + " alert");
+        }
+    }
+
+    public void transmitAlerts()
+    {
+        // You can transmit 4 kinds of Alerts. INFO, WARNING, ERROR and CRITICAL.You can also transmit parameters along
+        // with the alert that will provide additional information about why the alert was generated.
+        transmitAlert(AlertType.INFO, "Everything seems okay");
+        transmitAlert(AlertType.WARNING, "An issue could occur");
+        transmitAlert(AlertType.ERROR, "An issue has happened");
+        transmitAlert(AlertType.CRITICAL, "A failure has happened");
+
+        // Wait a while to ensure that the alerts are transmitted.
+        try
+        {
+            Thread.currentThread().sleep(5000);
+        }
+        catch (InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+
+    private JSONArray createWaypoint(double latitude, double longitude)
+    {
         JSONArray waypoint = new JSONArray();
         waypoint.add(latitude);
         waypoint.add(longitude);
         return waypoint;
     }
 
-    private void transmitAlert(AlertType alertType) {
-        JSONObject data = new JSONObject();
-        data.put("demoKey", "demoValue");
+    private void transmitWaypoint() throws IllegalThingException
+    {
+        // Create a waypoint. A waypoint is a series of lat/long locations that a Thing can pass through.Currently we
+        // will just create a single waypoint. However you can also send a list of waypoints that a Thing could have
+        // passed through
+        JSONArray waypoint = createWaypoint(18.52, 73.85);
 
-        if (gateway.transmitAlert(thing.getKey(), alertType, "This is an example " + alertType.toString() + " alert!", data)) {
-            logger.info("Sent example " + alertType.toString() + " alert!");
-        } else {
-            logger.error("Could not send example " + alertType.toString() + " alert");
+        if (!gateway.transmitCompressedData(thing, null, waypoint))
+        {
+            logger.warn("Could not transmit a waypoint");
         }
+
     }
 
-    public void transmitDemoAlerts() {
-        transmitAlert(AlertType.INFO);
-        transmitAlert(AlertType.WARNING);
-        transmitAlert(AlertType.ERROR);
-        transmitAlert(AlertType.CRITICAL);
+    private void stopGateway()
+    {
+        gateway.stop();
     }
 
-    public void transmitData() throws IllegalThingException {
-        int heartbeat = 0;
-        for (int count = 1; count <= NUM_EVENTS; count++) {
-            // Construct the JSON packet to be sent. This has to match the
-            // metadata structure.
-            JSONObject data = getMetric();
-            //un-comment below line to create waypoints.
-            //JSONArray waypoint = createWaypoint(18.52 + Math.random(), 73.85 + Math.random());
-            
-            // Transmit the data and waypoint. There is also a method to
-            // specify the timestamp of the data packet if the transmission is delayed.
-            // The syntax is gateway.transmitData(thing, data, waypoint, timestamp)
-            // You can transmit, either data or waypoint, or both. Atleast one should be present
-            // from data and waypoint. Pass data or waypoint as null, if you don't want to send it.
-            // Following are three ways to transmit data.
-            // 1st way
-            if (!gateway.transmitCompressedData(thing, data, null)) {
-                logger.warn("Could not transmit packet : " + count + " value " + data.toJSONString());
-            }
-            // 2nd way. Note un-comment the commented code for creating the waypoint
-            //if (!gateway.transmitData(thing, null, waypoint)) {
-            //    logger.warn("Could not transmit packet : " + count + " waypoint " + waypoint.toJSONString());
-            //}
-            
-            // 3rd way. Note un-comment the commented code for creating the waypoint
-            //if (!gateway.transmitData(thing, data, waypoint)) {
-            //    logger.warn("Could not transmit packet : " + count + " data: " + data.toJSONString()  + ", waypoint: " + waypoint.toJSONString());
-            //}
-      
-    
-            heartbeat++;
-            if (heartbeat == 300) {
-                gateway.transmitHeartbeat();
-                heartbeat = 0;
-            }
-            try {
-                Thread.currentThread().sleep(10000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-        }
-    }
 }
-
